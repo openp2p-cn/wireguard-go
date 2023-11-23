d3c99446a4ae74de48162463fb6dae57b515a39f
TenderIronh <openp2p.cn@gmail.com>
2023-11-23 15:02:47 +0800

    disable IFF_VNET_HDR

--- tun/tun_linux.go
+++ tun/tun_linux.go
@@ -550,7 +550,8 @@ func CreateTUN(name string, mtu int) (Device, error) {
 	}
 	// IFF_VNET_HDR enables the "tun status hack" via routineHackListener()
 	// where a null write will return EINVAL indicating the TUN is up.
-	ifr.SetUint16(unix.IFF_TUN | unix.IFF_NO_PI | unix.IFF_VNET_HDR)
+	// ifr.SetUint16(unix.IFF_TUN | unix.IFF_NO_PI | unix.IFF_VNET_HDR)
+	ifr.SetUint16(unix.IFF_TUN | unix.IFF_NO_PI)
 	err = unix.IoctlIfreq(nfd, unix.TUNSETIFF, ifr)
 	if err != nil {
 		return nil, err
